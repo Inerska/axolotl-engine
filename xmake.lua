fcca6d2c89ae1017ba93b486bc95224e072dfce6
Alexis C. Gridel <alexis.gridel@pm.me>
2022-05-15 09:06:50 +0200

    feat(xrepo): setup flecs init based on conan packages center lts

--- xmake.lua
+++ xmake.lua
@@ -1,5 +1,11 @@
 add_rules("mode.debug", "mode.release")
 
+-- requires
+add_requires("conan::flecs/2.4.8")
+
 target(".")
     set_kind("binary")
     add_files("src/*.cpp")
+
+    -- packages
+    add_packages("conan::flecs/2.4.8")
