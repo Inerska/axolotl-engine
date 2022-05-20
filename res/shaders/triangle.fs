9dc5b2fadc064f825d72055a840897c479bcbd04
Alexis C. Gridel <alexis.gridel@pm.me>
2022-05-20 10:12:34 +0200

    has(samples): instable wiip

---
+++ res/shaders/triangle.fs
@@ -0,0 +1,6 @@
+﻿#version latest core
+out vec4 color;
+void main()
+{
+    color = vec4(0.2, 0.8, 0.2, 1.0);
+}
---
+++ samples/basic_window.cpp
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Alexis Chân Gridel. All Rights Reserved.
+// Copyright (c) Axolotl Engine contributors. All Rights Reserved.
+// Licensed under the GNU General Public License v3.0.
+// See the LICENSE file in the project root for more information.
+
+#include "../src/wt/glfw_wrapper.h"
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "sample hello-window" << std::endl;
+
+    ax::wt::window_t window = ax::wt::window_t(640, 480, "Hello World");
+
+    while (!window.should_close())
+    {
+        window.swap_buffers();
+        glfwPollEvents();
+    }
+
+    return 0;
+}
