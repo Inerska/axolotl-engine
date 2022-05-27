af81ef32645bf5aeffdcda52015c42ca4748ea54
Alexis C. Gridel <alexis.gridel@pm.me>
2022-05-27 10:14:40 +0200

    feat(test): logging core

---
+++ test/logging/logging_core_tests.cpp
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Alexis Chân Gridel. All Rights Reserved.
+// Copyright (c) Axolotl Engine contributors. All Rights Reserved.
+// Licensed under the GNU General Public License v3.0.
+// See the LICENSE file in the project root for more information.
+
+
+
+#include <gtest/gtest.h>
+#include "logging/logging_core.h"
+
+int main(){
+
+    ax::logging::log_t log;
+
+    return 0;
+}
+
+
+
